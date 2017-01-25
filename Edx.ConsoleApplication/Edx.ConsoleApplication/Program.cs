ef74acd47216f1deb961f5343897c824bc5700b7
lpgoncalves <lp.gooncalves@gmail.com>
2017-01-24 22:07:14 -0200

    First Commit

---
+++ Edx.ConsoleApplication/Edx.ConsoleApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using Edx.ConsoleApplication.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edx.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student() 
+            { 
+                AddressLine1 = "Higino Ribeiro Carvalho", 
+                Birthdate = "21/03/1993" 
+            });
+
+            students.Add(new Student()
+            {
+                AddressLine1 = "Higino Ribeiro Carvalho",
+                Birthdate = "21/03/1993"
+            });
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("First Name: {0}", student.FirstName);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EDX Programming - Practice Code
+Microsoft: DEV204x Programming with C#.
